d24612b60f38b0066a590300640e4711bf774119
kavya <potturilakshmikavya@gmail.com>
2025-04-23 22:45:46 +0530

    updated

--- BMI_calculator/.prettierrc.js
+++ BMI_calculator/.prettierrc.js
@@ -1,4 +1,11 @@
+// prettier.config.js or .prettierrc.js
 module.exports = {
-  semi: true,
-  singleQuote: true
+	trailingComma: 'es5',
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	'editor.formatOnSave': true,
+	// printWidth: 80,
+	proseWrap: 'always',
 };
